34092d5a002c243a107648f968007fad8524f6de
Eirik Vale Aase <e.v.aase@gmail.com>
2019-03-30 15:41:09 +0100

    Data sent to MQTT is shown in the graph template

--- src/server.py
+++ src/server.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, redirect, url_for
+import eventlet
+from flask import Flask, render_template
 from flask_mqtt import Mqtt
+from flask_socketio import SocketIO
+
+eventlet.monkey_patch()
 
 app = Flask(__name__, template_folder='./views')
-app.config['MQTT_BROKER_URL'] = '129.241.208.68'
+app.config['MQTT_BROKER_URL'] = 'localhost'
 app.config['MQTT_BROKER_PORT'] = 1883
 app.config['MQTT_REFRESH_TIME'] = 1.0
 
 mqtt = Mqtt(app)
+socketio = SocketIO(app)
 
 
 @mqtt.on_connect()
@@ -20,14 +25,19 @@ def handle_mqtt_message(client, userdata, message):
         topic=message.topic,
         payload=message.payload.decode()
     )
-
-    update_template()
-
-
-def update_template():
-    return redirect(url_for('.index', val="HEY"))
+    # emit a mqtt_message event to the socket containing the message data
+    socketio.emit('mqtt_message', data=data)
 
 
 @app.route('/')
 def index():
     return render_template('graph.html')
+
+
+@mqtt.on_log()
+def handle_logging(client, userdata, level, buf):
+    print(level, buf)
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='localhost', port=5000, debug=True)
