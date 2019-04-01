670733f611a5d63ea6c789799a9d0e1415da3e45
dammen <jodammen93@gmail.com>
2019-04-01 07:38:53 +0000

    setup on rpi

--- src/server.py
+++ src/server.py
@@ -40,4 +40,5 @@ def handle_logging(client, userdata, level, buf):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='localhost', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8000)
+    
