e897abc3d8eba21358c4e19e48413be3db1f576d
Eirik Vale Aase <e.v.aase@gmail.com>
2019-04-05 14:13:34 +0200

    Reformat code

--- src/server.py
+++ src/server.py
@@ -3,12 +3,10 @@ from flask import Flask, session, render_template, render_template_string
 from flask_mqtt import Mqtt
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
-#from users import User
+# from users import User
 from flask_user import login_required, UserManager, UserMixin, current_user
 
-
 eventlet.monkey_patch()
-
 
 app = Flask(__name__, template_folder='./views')
 app.config['MQTT_BROKER_URL'] = '129.241.208.68'
@@ -18,15 +16,16 @@ app.config['USER_ENABLE_EMAIL'] = False
 app.config['USER_ENABLE_USERNAME'] = True
 app.config['USER_REQUIRE_RETYPE_PASSWORD'] = False
 app.config['SECRET_KEY'] = 'This is an INSECURE secret!! DO NOT use this in production!!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quickstart_app.sqlite'    # File-based SQL database
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Avoids SQLAlchemy warning
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quickstart_app.sqlite'  # File-based SQL database
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Avoids SQLAlchemy warning
 app.config['USER_APP_NAME'] = "YOUR SLEEP MONITOR"
 mqtt = Mqtt(app)
 socketio = SocketIO(app)
 
 ### DB setup ###
- # Initialize Flask-SQLAlchemy
+# Initialize Flask-SQLAlchemy
 db = SQLAlchemy(app)
+
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
@@ -37,14 +36,16 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(255), nullable=False, server_default='')
     email_confirmed_at = db.Column(db.DateTime())
 
-    #User information
+    # User information
     first_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
     last_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
+
 
 # Create all database tables
 db.create_all()
 # Setup Flask-User and specify the User data-model
 user_manager = UserManager(app, db, User)
+
 
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
@@ -64,6 +65,7 @@ def handle_mqtt_message(client, userdata, message):
 @app.route('/')
 def home_page():
     return render_template('home_page.html', current_user=current_user)
+
 
 @app.route('/user/<username>')
 @login_required
@@ -85,4 +87,3 @@ def handle_logging(client, userdata, level, buf):
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=8000)
-    
